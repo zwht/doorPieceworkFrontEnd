58c284c4f86f7e497efc776c9768750200abef74
zhaowei <1512763623@qq.com>
2018-03-14 21:32:45 +0800

    dhdh

--- src/app/main/component/app/app.component.ts
+++ src/app/main/component/app/app.component.ts
@@ -1,16 +1,42 @@
-import { Component } from '@angular/core';
-import {SetCodeService} from "../../../common/service/set-code.service";
+import {Component} from '@angular/core';
+import {SetCodeService} from '../../../common/service/set-code.service';
+import {NavigationEnd, Router, ActivatedRoute} from '@angular/router';
+import {Title} from '@angular/platform-browser';
+import 'rxjs/add/operator/filter';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
-	constructor(private setCodeService:SetCodeService) {
+	title = 'app works!';
+
+	constructor(private setCodeService: SetCodeService,
+	            private titleService: Title,
+	            private router: Router,
+	            private activatedRoute: ActivatedRoute) {
 	}
+
 	ngOnInit() {
 		this.setCodeService.init();
+		this.router.events
+			.filter(event => event instanceof NavigationEnd)
+			.map(() => this.activatedRoute)
+			.map(route => {
+				while (route.firstChild) {
+					route = route.firstChild;
+				}
+				return route;
+			})
+			.map(route => route.data)
+			.subscribe((event) => {
+				if (event['value'] && event['value'].name) {
+					this.titleService.setTitle('计件_' + event['value'].name);
+				} else {
+					this.titleService.setTitle('页面不存在');
+				}
+
+			});
 	}
 }
